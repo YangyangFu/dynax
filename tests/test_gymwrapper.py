31643e1a35ff8a98195145c0cf39cf1e4cb0c1dd
yangyang Fu <fuyy2008@gmail.com>
2023-09-19 20:49:48 +0000

    added vectorized test

--- tests/test_gymwrapper.py
+++ tests/test_gymwrapper.py
@@ -2,127 +2,49 @@ import jax
 import jax.numpy as jnp 
 import flax.linen as nn
 from dynax.wrapper.envs.rc import RC 
-import time 
 
 # setup
 ts = 0
 te = 60*60
 dt = 60
 
-# ================================
-#        NO JIT
-# ================================
+# initialize a random key to cotnrol randomness
+key = jax.random.PRNGKey(0) 
+
 # instantiate an env
 env = RC(start_time=ts, end_time=te, dt=dt, num_actions=11, name='RC-V1')
-key = jax.random.PRNGKey(0)
-t = ts 
 
+# reset the environment with default parameters
 obs, states, params = env.reset(key)
-init_params = env.init(key, 0, states)
 
-start_time = time.time()
-# main loop
-while t < te:
-    key, key_reset = jax.random.split(key)
-    action = env.action_space.sample(key)
-    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
-    t += dt
-print("non-jittable run time: ", time.time() - start_time)
+# sample an action
+action = env.action_space.sample(key)
 
-"""
-# test jittable conditions
+assert action == 7
+assert env.action_space.contains(action)
+
+# step the environment
+obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
+
+## jit the env
+# NOTE: this is not as fast as expected
 env = nn.jit(RC)(start_time=ts, end_time=te, dt=dt, num_actions=11, name='RC-V1')
-t = ts 
-
 obs, states, params = env.reset(key)
-init_params = env.init(key, 0, states)
+action = env.action_space.sample(key)
+obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
 
-start_time = time.time()
-# main loop
-while t < te:
-    key, key_reset = jax.random.split(key)
-    action = env.action_space.sample(key)
-    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
-    print(action, info)
-    t += dt
 
-print("jittable run time: ", time.time() - start_time)
-"""
-
-# ================================
-#        JIT 1: jit step() only
-# ================================
-# test jittable conditions
+## jit the step: 10-20X speedup
 env = RC(start_time=ts, end_time=te, dt=dt, num_actions=11, name='RC-V1')
+obs, states, params = jax.jit(env.reset)(key)
+action = env.action_space.sample(key)
+obs_next, reward, terminated, truncated, info, states = jax.jit(env.step)(action, states=states, params=params)
 
-t = ts 
-obs, states, params = env.reset(key)
-init_params = env.init(key, 0, states)
-start_time = time.time()
+## vectorized env
+obs_res, states_res, params_res = jax.vmap(env.reset)(jax.random.split(key, 10)) 
+print(obs_res.shape, states_res.time.shape)
 
-# main loop
-while t < te:
-    key, key_reset = jax.random.split(key)
-    action = env.action_space.sample(key)
-    obs_next, reward, terminated, truncated, info, states = jax.jit(env.step)(action, states=states, params=params)
-    t += dt
-
-print("jittable 1 run time: ", time.time() - start_time)
-
-# ================================
-#       JIT 2: use jax.lax.scan with a rollout function
-# ================================
-
-env = RC(start_time=ts, end_time=te, dt=dt, num_actions=12, name='RC-V1')
-t = ts 
-obs, states, params = env.reset(key)
-
-def rollout(carry, keys):
-    step, states = carry 
-    action = env.action_space.sample(keys)
-    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
-    
-    step += 1
-
-    return (step, states), (action, states.x)
-
-key, *keys = jax.random.split(key, num=61)
-carry = (env.start_time, states)
-#scan = jax.lax.scan(rollout,
-#                variable_broadcast='params',
-#                split_rngs={'params':False},
-#                in_axes=0,
-#                out_axes=0,
-#                )
-
-start_time = time.time()
-_, xsol = jax.lax.scan(rollout, carry, jnp.array(keys))
-print("jittable 2 run time: ", time.time() - start_time)
-actions, xs = xsol
-print(actions)
-
-# ================================
-#      JIT 3: use nn.scan with a rollout function
-# nn.scan seems only works inside a nn.Module
-# ================================
-env = RC(start_time=ts, end_time=te, dt=dt, num_actions=12, name='RC-V1')
-t = ts
-obs, states, params = env.reset(key)
-
-def rollout(env, carry, inputs):
-    t, states = carry 
-    action = env.action_space.sample(inputs)
-    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
-    t += env.dt
-
-    return (t, states), (action, states.x)
-
-key, *keys = jax.random.split(key, num=61)
-carry = (env.start_time, states)
-print(params)
-
-scan = nn.scan(rollout,
-                in_axes=0,
-                out_axes=0,
-                )
-carry, xsol = scan(env, carry, jnp.array(keys))
+action = jax.vmap(env.action_space.sample)(jax.random.split(key, 20))
+print(action.shape)
+obs_next, reward, terminated, truncated, info, states = jax.vmap(env.step)(action, states=states, params=params)
+print(obs_next.shape)
---
+++ tests/test_gymwrapper_jittable.py
@@ -0,0 +1,128 @@
+import jax
+import jax.numpy as jnp 
+import flax.linen as nn
+from dynax.wrapper.envs.rc import RC 
+import time 
+
+# setup
+ts = 0
+te = 60*60
+dt = 60
+
+# ================================
+#        NO JIT
+# ================================
+# instantiate an env
+env = RC(start_time=ts, end_time=te, dt=dt, num_actions=11, name='RC-V1')
+key = jax.random.PRNGKey(0)
+t = ts 
+
+obs, states, params = env.reset(key)
+init_params = env.init(key, 0, states)
+
+start_time = time.time()
+# main loop
+while t < te:
+    key, key_reset = jax.random.split(key)
+    action = env.action_space.sample(key)
+    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
+    t += dt
+print("non-jittable run time: ", time.time() - start_time)
+
+"""
+# test jittable conditions
+env = nn.jit(RC)(start_time=ts, end_time=te, dt=dt, num_actions=11, name='RC-V1')
+t = ts 
+
+obs, states, params = env.reset(key)
+init_params = env.init(key, 0, states)
+
+start_time = time.time()
+# main loop
+while t < te:
+    key, key_reset = jax.random.split(key)
+    action = env.action_space.sample(key)
+    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
+    print(action, info)
+    t += dt
+
+print("jittable run time: ", time.time() - start_time)
+"""
+
+# ================================
+#        JIT 1: jit step() only
+# ================================
+# test jittable conditions
+env = RC(start_time=ts, end_time=te, dt=dt, num_actions=11, name='RC-V1')
+
+t = ts 
+obs, states, params = env.reset(key)
+init_params = env.init(key, 0, states)
+start_time = time.time()
+
+# main loop
+while t < te:
+    key, key_reset = jax.random.split(key)
+    action = env.action_space.sample(key)
+    obs_next, reward, terminated, truncated, info, states = jax.jit(env.step)(action, states=states, params=params)
+    t += dt
+
+print("jittable 1 run time: ", time.time() - start_time)
+
+# ================================
+#       JIT 2: use jax.lax.scan with a rollout function
+# ================================
+
+env = RC(start_time=ts, end_time=te, dt=dt, num_actions=12, name='RC-V1')
+t = ts 
+obs, states, params = env.reset(key)
+
+def rollout(carry, keys):
+    step, states = carry 
+    action = env.action_space.sample(keys)
+    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
+    
+    step += 1
+
+    return (step, states), (action, states.x)
+
+key, *keys = jax.random.split(key, num=61)
+carry = (env.start_time, states)
+#scan = jax.lax.scan(rollout,
+#                variable_broadcast='params',
+#                split_rngs={'params':False},
+#                in_axes=0,
+#                out_axes=0,
+#                )
+
+start_time = time.time()
+_, xsol = jax.lax.scan(rollout, carry, jnp.array(keys))
+print("jittable 2 run time: ", time.time() - start_time)
+actions, xs = xsol
+print(actions)
+
+# ================================
+#      JIT 3: use nn.scan with a rollout function
+# nn.scan seems only works inside a nn.Module
+# ================================
+env = RC(start_time=ts, end_time=te, dt=dt, num_actions=12, name='RC-V1')
+t = ts
+obs, states, params = env.reset(key)
+
+def rollout(env, carry, inputs):
+    t, states = carry 
+    action = env.action_space.sample(inputs)
+    obs_next, reward, terminated, truncated, info, states = env.step(action, states=states, params=params)
+    t += env.dt
+
+    return (t, states), (action, states.x)
+
+key, *keys = jax.random.split(key, num=61)
+carry = (env.start_time, states)
+print(params)
+
+scan = nn.scan(rollout,
+                in_axes=0,
+                out_axes=0,
+                )
+carry, xsol = scan(env, carry, jnp.array(keys))
